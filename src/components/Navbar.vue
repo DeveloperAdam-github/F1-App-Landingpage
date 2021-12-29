7dac3876f5e69f6a3fef179e1ae8e9d1e9f15a12
Adam Powell <adam@ecce.uk>
2021-12-29 13:52:34 +0000

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="bg-primary dark:bg-secondary text-secondary dark:text-primary h-10vh w-full border-b md:border-0 border-gray-400 flex font-boldHeadline text-base lg:text-xl justify-between px-4 md:px-10 items-center"
+  >
+    <div>
+      <h1>F1 Stats<i class="text-purple fas fa-flag-checkered ml-2"></i></h1>
+    </div>
+    <div class="w-2/5 lg:w-1/4 flex justify-between items-center">
+      <router-link class="" :to="{ name: 'Home' }">Home</router-link>
+      <router-link class="" :to="{ name: 'Contact' }">Contact</router-link>
+      <!-- <div
+        class="h-9 w-9 rounded-full bg-secondary dark:bg-primary flex items-center justify-center transform cursor-pointer transition-all"
+      >
+        <i
+          v-if="isDarkModeActive"
+          @click="
+            $emit('darkMode'), (this.isDarkModeActive = !this.isDarkModeActive)
+          "
+          class="fas fa-sun text-lg font-bold text-primary dark:text-secondary"
+        ></i>
+        <i
+          v-if="!isDarkModeActive"
+          @click="
+            $emit('darkMode'), (this.isDarkModeActive = !this.isDarkModeActive)
+          "
+          class="fas fa-moon text-lg font-bold text-primary dark:text-secondary"
+        ></i>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDarkModeActive: false,
+    };
+  },
+};
+</script>
+
+<style>
+.router-link-active {
+  color: #ee0000;
+  font-weight: bold;
+
+  transition: all 0.3s linear;
+}
+
+.router-link-active span {
+  opacity: 100;
+  color: #ee0000;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,128 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div
+      class="flex flex-col lg:grid lg:grid-cols-2 lg:grid-rows-2 items-center p-2 md:p-10"
+    >
+      <div class="flex justify-center">
+        <div
+          class="w-82 2xl:w-full flex px-auto items-center justify-center flex-col text-center m-10 dark:text-primary"
+        >
+          <h1 class="font-boldHeadline text-2xl w-full">LIVE Standings</h1>
+          <p class="font-headline text-sm dark:text-gray-500 mt-5">
+            Stay up to date with the latest driver and constructor standings in
+            the current championship battles.
+          </p>
+
+          <div class="flex flex-col">
+            <router-link class="flex items-start" :to="{ name: 'ComingSoon' }">
+              <button
+                class="m-10 flex w-52 text-white bg-purple border-0 py-2 px-4 focus:outline-none items-center text-left hover:bg-red-700 rounded text-xs font-headline"
+              >
+                <div class="h-full">
+                  <i class="fab fa-apple text-3xl px-2.5"></i>
+                </div>
+                <div class="flex flex-col w-2/3">
+                  <p class="">Download on the</p>
+                  <p class="text-lg font-bold">App Store</p>
+                </div>
+              </button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div class="mockup-phone border-primary">
+          <div class="camera"></div>
+          <div class="display">
+            <div class="artboard phone-1 artboard-demo bg-light dark:bg-dark">
+              <img
+                class="w-full object-cover pt-28"
+                src="../assets/screenshot7.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--  -->
+      <div
+        class="flex lg:hidden w-82 2xl:w-full px-auto items-center justify-center flex-col text-center m-10 dark:text-primary"
+      >
+        <h1 class="font-boldHeadline text-2xl w-full">Head to Head</h1>
+        <p class="font-headline text-sm dark:text-gray-500 mt-5">
+          Check out how the drivers stack up against one another within each
+          team,
+          <span>compare their stats too see who is coming out on top!</span>
+        </p>
+
+        <div class="flex flex-col"></div>
+      </div>
+
+      <div class="m-10">
+        <div class="mockup-phone border-primary">
+          <div class="camera"></div>
+          <div class="display">
+            <div class="artboard phone-1 artboard-demo bg-light dark:bg-dark">
+              <img class="dark" src="../assets/screenshot.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="hidden lg:flex w-82 2xl:w-full px-auto items-center justify-center flex-col text-center m-10 dark:text-primary"
+      >
+        <h1 class="font-boldHeadline text-2xl w-full">Head to Head</h1>
+        <p class="font-headline text-sm dark:text-gray-500 mt-5">
+          Check out how the drivers stack up against one another within each
+          team,
+          <span>compare their stats too see who is coming out on top!</span>
+        </p>
+
+        <div class="flex flex-col"></div>
+      </div>
+    </div>
+    <div></div>
+
+    <Footer class="bg-primary dark:bg-secondary" />
   </div>
 </template>
 
 <script>
+import Footer from '../components/Footer.vue';
+import { mapGetters } from 'vuex';
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      toggleDarkModeFalse: false,
+    };
+  },
+  components: { Footer },
+  watch() {
+    this.$store.state.darkMode;
+  },
+  computed: {
+    ...mapGetters(['toggleDarkModeFalse', 'toggleDarkModeTrue']),
+  },
+};
 </script>
+
+<style scoped>
+button:hover p {
+  display: none;
+}
+
+button:hover:before {
+  content: 'Coming Soon!';
+  height: 45px;
+  display: flex;
+  justify-content: center;
+  place-items: center;
+  padding: 10px;
+  font-size: 20px;
+  text-decoration: underline;
+  line-height: 20px;
+}
+</style>
